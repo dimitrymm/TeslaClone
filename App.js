68c9f3e4dcaaf10c7d6a2c9c3311d0662b4d04a9
Dimitry Machado Marinho <dimitrym.marinho@gmail.com>
2021-03-21 20:04:52 -0300

    Text Inicial

--- App.js
+++ App.js
@@ -5,7 +5,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World Dimitry!</Text>
+      
+      <View style={styles.carContainer}>
+        <View style={styles.titlesZ}>
+          <Text style={styles.title}>Projeto</Text>
+          <Text style={styles.subtitle}>Planejador Financeiro</Text>
+        </View>
+
+      </View>
+
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer:{
+    width:'100%',
+    height:'100%',
+  },
+  titlesZ:{
+    marginTop:'30%',
+    width:'100%',
+    alignItems:'center',
+
+  },
+  title:{
+    fontSize:40,
+    fontWeight:'600',
+  },
+  subtitle:{
+    fontSize:16,
+    color:'#028968'
+  }
+  
+
+
+
 });
